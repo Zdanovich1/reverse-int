2a7b5b8e822c6e64eca8c5414dfb2c1706e06837
Zdanovich1 <zdanovich.fti@gmail.com>
2022-01-12 14:57:21 +0300

    feet:complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   if (n < 0) {
+      let b = n.toString().split('').reverse()
+      b.pop();
+      return +b.join('');
+   } else {
+      let b = n.toString().split('').reverse().join('');
+      return +b;
+   }
 }
